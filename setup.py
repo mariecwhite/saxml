d9b68253cf6b1121264b29c9e7a6327666f7ad8c
mariecwhite <mariewhite@google.com>
2024-07-01 05:52:48 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,66 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Setup.py file for saxml."""
+
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+# Set this envvar to avoid installing packages from head that can overwrite
+# existing installs of those packages, e.g., jax
+SKIP_HEAD_INSTALLS = os.environ.get('SKIP_HEAD_INSTALLS', '')
+
+def _get_requirements():
+  """Parses requirements.txt file."""
+  install_requires_tmp = []
+  with open(
+      os.path.join(os.path.dirname(__file__), './requirements.in'), 'r'
+  ) as f:
+    for line in f:
+      package_name = line.strip()
+      # Skip empty line or comments starting with "#".
+      if (
+          not package_name
+          or package_name[0] == '#'
+          or package_name.startswith('--find-links')
+          or (' @ ' in package_name and SKIP_HEAD_INSTALLS)
+      ):
+        continue
+      else:
+        install_requires_tmp.append(package_name)
+  return install_requires_tmp
+
+
+install_requires = _get_requirements()
+
+setup(
+    name='saxml',
+    version='1.2.0',
+    description=(
+        'An experimental system that serves Paxml, JAX, and PyTorch models for inference.'
+    ),
+    author='SAXML team',
+    author_email='saxml-dev@google.com',
+    packages=find_packages(),
+    python_requires='>=3.10',
+    install_requires=install_requires,
+    url='https://github.com/google/saxml',
+    license='Apache-2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    zip_safe=False,
+)
